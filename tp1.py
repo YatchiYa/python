27b68171c792c7627321a9f6c578d5f925f258e7
Youcef ARAB <ii12562@etu.parisdescartes.fr>
2020-08-25 01:51:50 +0200

    de

--- tp1.py
+++ tp1.py
@@ -7,8 +7,9 @@ print ("calcule de l'air de la surface  d'un rectangle")
 
 # on saisie la largeur et la hauteur de type flotante
 a = float(input("largeur = "))
-b = float(input("hauteur = "))
+b = float(input("longeur = "))
 
 # calcule de l'air
 air = a * b
+
 print ("Air de la surface du rectangle = ", air)
--- tp3.py
+++ tp3.py
@@ -4,17 +4,20 @@
 import math
 
 # affichage 
-print "calcule du quotient et du resultat"
+print ("calcule du quotient et du resultat")
+print ("")
 
 # on saisie a et b
+
+print (" saisie de a et b")
 a = int(input("a = "))
 b = int(input("b = "))
 
 # calcule du quotient a % b
-quotient = a % b
+quotient = int(a % b)
 # calcule du resultat a / b
-resultat = a / b
+resultat = int(a / b)
 
 # affichage
 print ("affichage : ")
-print a, "=", b,"*",resultat, "+", quotient
+print (a, "=", b,"*", resultat, "+", quotient)
