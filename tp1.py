889ce01216d56cf14fcb66c7cb397c0219e1d7ba
YatchiYa <yarab@student.42.fr>
2020-08-24 07:08:49 +0200

    de

---
+++ tp1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# Import modules
+import math
+
+print "calcule de l'air de la surface  d'un rectangle"
+
+# on saisie la largeur et la hauteur de type flotante
+a = float(input("largeur = "))
+b = float(input("hauteur = "))
+
+# calcule de l'ai
+air = a * b
+print ("Air de la surface du rectangle = ", air)
---
+++ tp3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# Import modules
+import math
+
+print "calcule du quotient et du resultat"
+
+# on saisie a et b
+a = int(input("a = "))
+b = int(input("b = "))
+
+# calcule du quotient a % b
+quotient = a % b
+# calcule du resultat a / b
+resultat = a / b
+
+# affichage
+print ("affichage : ")
+print a, "=", b,"*",resultat, "+", quotient
